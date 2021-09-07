80aff517de8885d33b4398527f4c6f2ee19c8635
LuoGao97 <2374359479@qq.com>
2021-09-07 17:21:14 +0800

    commit

--- src/main/java/com/demo001/pojo/Department.java
+++ src/main/java/com/demo001/pojo/Department.java
@@ -1,13 +1,18 @@
 package com.demo001.pojo;
 
 import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Department
  * @author 
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Department implements Serializable {
     private Integer id;
 
--- src/main/java/com/demo001/pojo/Employee.java
+++ src/main/java/com/demo001/pojo/Employee.java
@@ -1,13 +1,18 @@
 package com.demo001.pojo;
 
 import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Employee
  * @author 
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee implements Serializable {
     private Integer id;
 
